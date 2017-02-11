819dd3c7b216372b28429f726354f0fb55da4ad2
Joshua-PC\josh <joshhpatriquin@gmail.com>
2017-02-10 21:38:21 -0400

    Adding Mock UI scene.

---
+++ CrowdCOREGame/Assets/Artwork/UI/UIAnimations/MockMenuTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MockMenuTest : MonoBehaviour {
+
+	public Animator splashAnim;
+	public Animator textAnim;
+
+
+
+	void Update()
+	{
+		if(Input.GetKeyDown("space"))
+		{
+			this.gameObject.GetComponent<Animator>().SetTrigger("Switch");
+		}
+	}
+
+	public void SetOff()
+	{
+		splashAnim.SetTrigger("OffScreen");
+		textAnim.SetBool("OffScreen", true);
+	}
+
+	public void SetTitleOn()
+	{
+		splashAnim.SetTrigger("Title");
+		textAnim.SetBool("OffScreen", false);
+	}
+
+	public void SetControlsOn()
+	{
+		splashAnim.SetTrigger("Controls");
+	}
+}
