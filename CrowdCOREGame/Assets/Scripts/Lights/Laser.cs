c1fd75bbd7fde8e418b5afbfa5fcfb4cd89d8c05
SpencerCongdon <spencer.congdon@gmail.com>
2017-03-19 22:34:56 -0300

    Initial creation
    I've got a generator going with a simple laser script and prefab.

---
+++ CrowdCOREGame/Assets/Scripts/Lights/Laser.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// It's a laser
+/// </summary>
+[RequireComponent(typeof(LineRenderer))]
+public class Laser : MonoBehaviour
+{
+    private Coroutine flickerRoutine;
+
+    [SerializeField]
+    private LineRenderer line;
+
+    [SerializeField]
+    private bool flicker = false;
+    public float minOnInterval = .4f;
+    public float maxOnInterval = 2f;
+    public float minOffInterval = .4f;
+    public float maxOffInterval = 2f;
+
+    public float minWidth = .1f;
+    public float maxWidth = .4f;
+    public float widthTime = 0f;
+    public float widthDuration = 1f;
+    public bool isGrowing = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        if(line == null)
+        {
+            line = GetComponent<LineRenderer>();
+        }
+
+        Debug.Assert(line != null, this.name + " needs a line renderer");
+
+        line.startWidth = minWidth;
+        line.endWidth = minWidth;
+        widthTime = Random.Range(0, widthDuration); // Start at a random time, giving us a random width
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(flickerRoutine == null)
+        {
+            flickerRoutine = StartCoroutine(Flicker());
+        }
+
+        bool switchGrowth = (isGrowing) ? widthTime > widthDuration : widthTime < 0;
+        if (switchGrowth) isGrowing = !isGrowing;
+
+        widthTime += (isGrowing) ? Time.deltaTime : -Time.deltaTime;
+        float progress = widthTime / widthDuration;
+        progress = Mathf.Lerp(minWidth, maxWidth, progress);
+        line.startWidth = progress;
+        line.endWidth = progress;
+    }
+
+    public void SetLaser(Vector3 start, Vector3 end)
+    {
+        line.SetPosition(0, start);
+        line.SetPosition(1, end);
+    }
+
+    private IEnumerator Flicker()
+    {
+        yield return new WaitForSeconds(Random.Range(minOnInterval, maxOnInterval));
+        line.enabled = false;
+        yield return true;
+        line.enabled = true;
+        flickerRoutine = null;
+    }
+
+    private IEnumerator Flash()
+    {
+        yield return new WaitForSeconds(Random.Range(minOnInterval, maxOnInterval));
+        line.enabled = false;
+        yield return new WaitForSeconds(Random.Range(minOffInterval, maxOffInterval));
+        line.enabled = true;
+        flickerRoutine = null;
+    }
+
+}
---
+++ CrowdCOREGame/Assets/Scripts/Lights/LaserGenerator.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserGenerator : MonoBehaviour
+{
+    static Color gizmoColour = new Color(0.0f, 0.9f, 0.2f, 0.4f);
+
+    // The area to fill
+    public float HalfSizeX = 10f;
+    public float HalfSizeY = 10f;
+    public float HalfSizeZ = 10f;
+
+    public float MinSpacingX = 2f;
+    public float MinSpacingY = 2f;
+    public float MinSpacingZ = 2f;
+
+    public bool RandomSpacing = false;
+
+    // Whether or not to draw each axis
+    public bool AlignX = true;
+    public bool AlignY = false;
+    public bool AlignZ = false;
+
+    public Laser LaserPrefab;
+
+    // Use this for initialization
+    void Start ()
+    {
+        if(LaserPrefab != null)
+        {
+            GenerateLasers();
+        }
+        else
+        {
+            GameLog.LogWarning(this.name + " does not have a valid LaserPrefab assigned.", GameLog.Category.Lighting);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    }
+
+    public void SpawnNewLasers()
+    {
+        RemoveLasers();
+        GenerateLasers();
+    }
+
+    void GenerateLasers()
+    {
+        // Check to make sure we don't have an existing crowd
+        if(this.transform.childCount > 0)
+        {
+            GameLog.LogWarning("Cannot spawn lasers when spawner already has child objects", GameLog.Category.Lighting);
+            return;
+        }
+
+        // Place them along the X
+        if(AlignX)
+        {
+            // We need the distance along the X that we are rendering
+            float minX = transform.position.x - HalfSizeX;
+            float maxX = transform.position.x + HalfSizeX;
+
+            // All the values for controlling the extents of the lasers
+            float startZ = transform.position.z - HalfSizeZ;
+            float currentY = transform.position.y - HalfSizeY;
+            float currentZ = startZ;
+            float maxY = transform.position.y + HalfSizeY;
+            float maxZ = transform.position.z + HalfSizeZ;
+
+            // The vectors for the line
+            Vector3 start = new Vector3(minX, 0, 0);
+            Vector3 end = new Vector3(maxX, 0, 0);
+
+            while(currentY <= maxY)
+            {
+                start.y = currentY;
+                end.y = currentY;
+
+                while (currentZ <= maxZ)
+                {
+                    start.z = currentZ;
+                    end.z = currentZ;
+
+                    // Create and place laser
+                    Laser newLaser = Instantiate<Laser>(LaserPrefab);
+                    newLaser.transform.parent = this.transform;
+                    newLaser.SetLaser(start, end);
+
+                    // Update values
+                    // TODO: Randomize this
+                    currentZ += MinSpacingZ;
+                    
+                }
+
+                // TODO: Randomize this if necessary
+                currentY += MinSpacingY;
+                currentZ = startZ;
+            }
+        }
+
+    }
+
+    public void RemoveLasers()
+    {
+        var children = new List<GameObject>();
+        foreach (Transform child in transform) children.Add(child.gameObject);
+        foreach (var child in children) DestroyImmediate(child);
+        CrowdManager.Instance.CrowdCreated = false;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColour;
+        Gizmos.DrawCube(transform.position, new Vector3(HalfSizeX * 2, HalfSizeY * 2, HalfSizeZ * 2));
+    }
+}
